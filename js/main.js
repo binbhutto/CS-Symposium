1b84795956ca2856d2838580634f4484ea761883
Stas <stas.lyakhotskyy@gmail.com>
2021-02-12 18:00:05 +0200

    Add functional countdown

--- js/main.js
+++ js/main.js
@@ -8,53 +8,64 @@ https://templatemo.com/tm-526-vanilla
 
 jQuery(document).ready(function($) {
 
-	'use strict';
+    'use strict';
 
     var top_header = $('.parallax-content');
-    top_header.css({'background-position':'center center'}); // better use CSS
+    top_header.css({ 'background-position': 'center center' }); // better use CSS
 
-    $(window).scroll(function () {
-    var st = $(this).scrollTop();
-    top_header.css({'background-position':'center calc(50% + '+(st*.5)+'px)'});
+    $(window).scroll(function() {
+        var st = $(this).scrollTop();
+        top_header.css({ 'background-position': 'center calc(50% + ' + (st * .5) + 'px)' });
     });
 
 
-    $('body').scrollspy({ 
+    $('body').scrollspy({
         target: '.fixed-side-navbar',
         offset: 200
     });
-      
-      // smoothscroll on sidenav click
+
+    // smoothscroll on sidenav click
 
     $('.tabgroup > div').hide();
-        $('.tabgroup > div:first-of-type').show();
-        $('.tabs a').click(function(e){
-          e.preventDefault();
-            var $this = $(this),
-            tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+    $('.tabgroup > div:first-of-type').show();
+    $('.tabs a').click(function(e) {
+        e.preventDefault();
+        var $this = $(this),
+            tabgroup = '#' + $this.parents('.tabs').data('tabgroup'),
             others = $this.closest('li').siblings().children('a'),
             target = $this.attr('href');
         others.removeClass('active');
         $this.addClass('active');
         $(tabgroup).children('div').hide();
         $(target).show();
-      
+
     })
 
     var owl = $("#owl-testimonials");
 
-      owl.owlCarousel({
-        
-        pagination : true,
+    owl.owlCarousel({
+
+        pagination: true,
         paginationNumbers: false,
         autoPlay: 6000, //Set AutoPlay to 3 seconds
-        items : 3, //10 items above 1000px browser width
-        itemsDesktop : [1000,3], //5 items between 1000px and 901px
-        itemsDesktopSmall : [900,2], // betweem 900px and 601px
-        itemsTablet: [600,1], //2 items between 600 and 0
-        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
-        
+        items: 3, //10 items above 1000px browser width
+        itemsDesktop: [1000, 3], //5 items between 1000px and 901px
+        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
+        itemsTablet: [600, 1], //2 items between 600 and 0
+        itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
+
     });
 
+    // Functional countdown
+    var countDownDate = new Date("Apr 5, 2021 13:30").getTime();
+    var countDown = setInterval(function() {
+        var now = new Date().getTime();
 
+        var distance = countDownDate - now;
+
+        $("days")[0].textContent = Math.floor(distance / (1000 * 60 * 60 * 24));
+        $("hrs")[0].textContent = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        $("mins")[0].textContent = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        $("sec")[0].textContent = Math.floor((distance % (1000 * 60)) / 1000);
+    }, 1000);
 });
