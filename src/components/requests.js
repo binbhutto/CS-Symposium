72f1dd0e16a7e951e0173223c8c134830a3071de
hrk2023 <hrkdasgupta025@gmail.com>
2021-04-03 15:55:09 +0530

    Modified requests.js

--- src/components/requests.js
+++ src/components/requests.js
@@ -4,7 +4,7 @@ const BASE_URL = process.env.REACT_APP_BASE_URL
 export const requests = {
     fetchNextEvents : `${BASE_URL}api/nextevent`,
     fetchPastEvents : `${BASE_URL}api/pastevent`,
-    fetchBlog : `${BASE_URL}blog/`,
-    newBlog : `${BASE_URL}blog/add`,
+    fetchBlog : `${BASE_URL}b/`,
+    newBlog : `${BASE_URL}b/add`,
 }
 
